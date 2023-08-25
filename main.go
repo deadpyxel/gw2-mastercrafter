c8aa7e69fef958cddb58e40dd04f97c5bfd29b85
Robson Cruz <ghost0092@gmail.com>
2023-08-25 20:48:07 -0300

    feat(main.go): update wrapper code to use new methods

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/deadpyxel/gw2-mastercrafter/internal/config"
+	"github.com/deadpyxel/gw2-mastercrafter/pkg/api"
 )
 
 func main() {
-	fmt.Println("hello there///")
+	apiToken := os.Getenv("API_TOKEN")
+	if apiToken == "" {
+		config := config.ReadConfig()
+		apiToken = config.ApiKey
+	}
+	gw2Client := api.NewAPIClient("https://api.guildwars2.com/v2", apiToken)
+	recipeIds, err := gw2Client.FetchAvailableRecipesIds(46731)
+	if err != nil {
+		log.Fatalf("Error fetching Recipe Ids for process: %v\n", err)
+	}
+	fmt.Printf("recipeIds: %v\n", recipeIds)
 }
