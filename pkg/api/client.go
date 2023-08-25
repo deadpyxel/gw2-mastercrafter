d4f426788c59deaeb8e15d697f654395b3a30bb9
Robson Cruz <ghost0092@gmail.com>
2023-08-25 20:47:00 -0300

    feat(pks/api): add method to fetch recipe Ids based on itemID input

--- pkg/api/client.go
+++ pkg/api/client.go
@@ -1,6 +1,13 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/deadpyxel/gw2-mastercrafter/pkg/types"
+)
 
 type APIClient struct {
 	baseURL   string
@@ -11,7 +18,7 @@ func NewAPIClient(baseURL, authToken string) *APIClient {
 	return &APIClient{baseURL: baseURL, authToken: authToken}
 }
 
-func (client *APIClient) MakeRequest(endpoint string) (*http.Response, error) {
+func (client *APIClient) makeRequest(endpoint string) (*http.Response, error) {
 	url := client.baseURL + endpoint
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -20,4 +27,26 @@ func (client *APIClient) MakeRequest(endpoint string) (*http.Response, error) {
 	req.Header.Set("Authorization", "Bearer "+client.authToken)
 	httpClient := &http.Client{}
 	return httpClient.Do(req)
+}
+
+func (client *APIClient) FetchAvailableRecipesIds(itemID int) (types.RecipeIds, error) {
+	endpoint := fmt.Sprintf("/recipes/search?input=%d", itemID)
+	response, err := client.makeRequest(endpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var recipeIds types.RecipeIds
+	err = json.Unmarshal(body, &recipeIds)
+	if err != nil {
+		return nil, err
+	}
+
+	return recipeIds, nil
 }
