451c3026ed4647bc39f1ca11f0edcdd4e187ab86
Robson Cruz <ghost0092@gmail.com>
2023-08-28 19:46:35 -0300

    chore(main.go): update entrypoint

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/deadpyxel/gw2-mastercrafter/internal/config"
 	"github.com/deadpyxel/gw2-mastercrafter/pkg/api"
+	"github.com/deadpyxel/gw2-mastercrafter/pkg/crafting"
 )
 
 func main() {
@@ -16,7 +17,7 @@ func main() {
 		apiToken = config.ApiKey
 	}
 	gw2Client := api.NewAPIClient("https://api.guildwars2.com/v2", apiToken)
-	recipeIds, err := gw2Client.FetchAvailableRecipesIds(46731)
+	recipeIds, err := gw2Client.FetchAvailableRecipesIds(19700)
 	if err != nil {
 		log.Fatalf("Error fetching Recipe Ids for process: %v\n", err)
 	}
@@ -26,5 +27,13 @@ func main() {
 			log.Fatalf("Error fetching recipe data %v\n", err)
 		}
 		fmt.Printf("Recipe: %v\n", recipe)
+		item, err := gw2Client.FetchItem(recipe.OutputItemID)
+		if err != nil {
+			log.Fatalf("Error fetching item data %v\n", err)
+		}
+		fmt.Printf("Item Data: %v\n", item)
+
 	}
+	crafter := crafting.NewCrafter(*gw2Client)
+	crafter.FindProfitableOptions(19700)
 }
