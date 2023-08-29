149f01247296f98608b4bf1287b1ccc1df655bc6
Robson Cruz <ghost0092@gmail.com>
2023-08-29 13:01:25 -0300

    refactor(main.go): remove API code
    This will be called internally by other packages, so we don't want to have extra calls on the entrypoint

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/deadpyxel/gw2-mastercrafter/internal/config"
@@ -17,23 +15,6 @@ func main() {
 		apiToken = config.ApiKey
 	}
 	gw2Client := api.NewAPIClient("https://api.guildwars2.com/v2", apiToken)
-	recipeIds, err := gw2Client.FetchAvailableRecipesIds(19700)
-	if err != nil {
-		log.Fatalf("Error fetching Recipe Ids for process: %v\n", err)
-	}
-	for _, recipeId := range recipeIds {
-		recipe, err := gw2Client.FetchRecipe(recipeId)
-		if err != nil {
-			log.Fatalf("Error fetching recipe data %v\n", err)
-		}
-		fmt.Printf("Recipe: %v\n", recipe)
-		item, err := gw2Client.FetchItem(recipe.OutputItemID)
-		if err != nil {
-			log.Fatalf("Error fetching item data %v\n", err)
-		}
-		fmt.Printf("Item Data: %v\n", item)
-
-	}
 	crafter := crafting.NewCrafter(*gw2Client)
 	crafter.FindProfitableOptions(19700)
 }
