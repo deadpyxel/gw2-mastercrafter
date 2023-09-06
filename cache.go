2f7203977f1d8ec49df49c10cb1e052951f25cf0
Robson Cruz <ghost0092@gmail.com>
2023-09-06 13:54:32 -0300

    feat(cache.go): add recipe saving to local cache
    now recipes that have been fetch from the API are inserted/updated on the local DB cache

--- cache.go
+++ cache.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strings"
+	"sync"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -30,7 +32,10 @@ func UpdateCache(client *APIClient) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Recipe count: %d\n", len(recipes))
+		err = updateRecipeCache(db, recipes)
+		if err != nil {
+			log.Fatalf("Failure updating local recipe cache: %+v", err)
+		}
 		// update build number here
 		err = updateBuildMetadata(db, currentBuildMetadata)
 		if err != nil {
@@ -40,12 +45,25 @@ func UpdateCache(client *APIClient) {
 }
 
 func fetchStoredBuildNumber(db *sql.DB) (int, error) {
+	// Check if table already exists
+	var exists bool
+	err := db.QueryRow("SELECT EXISTS (SELECT name FROM sqlite_schema WHERE type='table' AND name='metadata')").Scan(&exists)
+	if err != nil {
+		return 0, fmt.Errorf("failed to check if table exists: %w", err)
+	}
+	if !exists {
+		// The table does not exist
+		return 0, nil
+	}
 	query := "SELECT build_number FROM metadata"
 	var currentBuildNumber int
-	err := db.QueryRow(query).Scan(&currentBuildNumber)
+	err = db.QueryRow(query).Scan(&currentBuildNumber)
 	if err != nil {
-		// TODO: better logic for cases where the table is empty or does not exist
-		return 0, nil // in case the table does not exist
+		if err == sql.ErrNoRows {
+			// The table is empty
+			return 0, nil
+		}
+		return 0, fmt.Errorf("Failed to query local cache for build number: %w", err)
 	}
 	return currentBuildNumber, err
 }
@@ -131,6 +149,108 @@ func updateBuildMetadata(db *sql.DB, buildMetadata Metadata) error {
 	query := "UPDATE metadata set build_number = ?"
 	_, err := db.Exec(query, buildMetadata.BuildNumber)
 	return err
+}
+
+func updateRecipeCache(db *sql.DB, recipes []Recipe) error {
+	createtableQuery := `
+    CREATE TABLE IF NOT EXISTS recipes (
+      id INTEGER PRIMARY KEY,
+      type TEXT,
+      output_item_id INTEGER,
+      output_item_count INTEGER,
+      disciplines TEXT,
+      min_rating INTEGER,
+      flags TEXT
+     );
+
+     CREATE TABLE IF NOT EXISTS ingredients (
+      id INTEGER PRIMARY KEY,
+      item_id INTEGER,
+      count INTEGER,
+      recipe_id INTEGER,
+      FOREIGN KEY(recipe_id) REFERENCES recipes(id),
+      UNIQUE(item_id, recipe_id)
+    );
+  `
+	_, err := db.Exec(createtableQuery)
+	if err != nil {
+		return err
+	}
+	// create transation object
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	// Use a waitGroup to ensure all upserts are done before commiting
+	var wg sync.WaitGroup
+	errorsChan := make(chan error, len(recipes))
+	upsertRecipeStmt := `
+		INSERT INTO recipes (id, type, output_item_id, output_item_count, disciplines, min_rating, flags)
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+		ON CONFLICT(id) DO UPDATE SET
+			type = excluded.type,
+			output_item_id = excluded.output_item_id,
+			output_item_count = excluded.output_item_count,
+			disciplines = excluded.disciplines,
+			min_rating = excluded.min_rating,
+			flags = excluded.flags;
+  `
+	upsertIngredientStmt := `
+		INSERT INTO ingredients (item_id, count, recipe_id)
+		VALUES (?, ?, ?)
+		ON CONFLICT(item_id, recipe_id) DO UPDATE SET
+			count = excluded.count
+  `
+	for _, recipe := range recipes {
+		wg.Add(1)
+		go func(recipe Recipe) {
+			defer wg.Done()
+
+			_, err := tx.Exec(upsertRecipeStmt,
+				recipe.ID,
+				recipe.Type,
+				recipe.OutputItemID,
+				recipe.OutputItemCount,
+				strings.Join(recipe.Disciplines, ","),
+				recipe.MinRating,
+				strings.Join(recipe.Flags, ","),
+			)
+			if err != nil {
+				errorsChan <- fmt.Errorf("Error upserting recipe %d: %w", recipe.ID, err)
+				return
+			}
+
+			for _, ingredient := range recipe.Ingredients {
+				_, err := tx.Exec(upsertIngredientStmt, ingredient.ItemID, ingredient.Count, recipe.ID)
+				if err != nil {
+					errorsChan <- fmt.Errorf("Error upserting ingredient %d for recipe %d: %w", ingredient.ItemID, recipe.ID, err)
+					return
+				}
+			}
+		}(recipe)
+	}
+
+	// Wait for all upserts to finish
+	wg.Wait()
+	close(errorsChan)
+	// Check if we had any errors during upsert
+	for err := range errorsChan {
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	// Commit transaction
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("Failed to commit transiction while updating recipe cache: %w", err)
+	}
+	return nil
+}
+
+func loadRecipeCache(db *sql.DB) []Recipe {
+	return []Recipe{}
 }
 
 func createMetadataTableIfNotExists(db *sql.DB) error {
