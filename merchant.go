09edf690aa5c1e2d0011baff0ed9c1760621d6a9
Robson Cruz <ghost0092@gmail.com>
2023-10-11 19:30:57 -0300

    feat(merchant.go): add missing struct tags to Merchant structs

--- merchant.go
+++ merchant.go
@@ -6,22 +6,22 @@ import (
 )
 
 type MerchantPrice struct {
-	Type  string `json:"type"`  // type of currency for payment (Item when it is an exchange)
-	ID    int    `json:"id"`    // item/currency id
-	Count int    `json:"count"` // count of item/currency to use
+	Type  string `json:"type" db:"type"`      // type of currency for payment (Item when it is an exchange)
+	ID    int    `json:"id" db:"currency_id"` // item/currency id
+	Count int    `json:"count" db:"count"`    // count of item/currency to use
 }
 
 type MerchantOptions struct {
-	Type   string          `json:"type"`             // type of item offering
-	ID     int             `json:"id"`               // Item id
-	Count  int             `json:"count"`            // count of item for sale
-	Price  []MerchantPrice `json:"price"`            // pricing options for the item
-	Ignore bool            `json:"ignore,omitempty"` // True if item should be ignored for processing (deprecated or unavailable)
+	Type   string          `json:"type" db:"type"`               // type of item offering
+	ID     int             `json:"id" db:"item_id"`              // Item id
+	Count  int             `json:"count" db:"count"`             // count of item for sale
+	Price  []MerchantPrice `json:"price" db:"price"`             // pricing options for the item
+	Ignore bool            `json:"ignore,omitempty" db:"ignore"` // True if item should be ignored for processing (deprecated or unavailable)
 }
 
 type Merchant struct {
 	Name            string            `json:"name" db:"name"`                           // Merchant name
-	Locations       []string          `json:"locations" db:"location"`                  // Merchant locations
+	Locations       StringSlice       `json:"locations" db:"location"`                  // Merchant locations
 	DisplayName     string            `json:"display_name,omitempty" db:"display_name"` // Display name for merchant
 	PurchaseOptions []MerchantOptions `json:"purchase_options"`                         // Offerings by merchant
 }
